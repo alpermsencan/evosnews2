9388d64cf2395e58e7f9e48036e90cb5721a4eb7
alpermsencan <alpermsencan@gmail.com>
2026-08-25 15:30:01 +0300

    Evos2

--- app/feed.xml/route.ts
+++ app/feed.xml/route.ts
@@ -54,4 +54,3 @@ ${items}
     },
   });
 }
-export const dynamic = "force-dynamic";
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -62,4 +62,3 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     })),
   ];
 }
-export const dynamic = "force-dynamic";
